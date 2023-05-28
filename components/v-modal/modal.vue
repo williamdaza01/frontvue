fcd1c63c9de0995ed955b423dc4fc91a44393c18
William Daza <williamdaza04@gmail.com>
2023-05-28 10:02:12 -0500

    arreglando modal

--- components/v-modal/modal.vue
+++ components/v-modal/modal.vue
@@ -1,71 +1,65 @@
 <template>
-    <v-list :class="listDensity" :items="items">
-      <v-list-item @click="handleItemClick(item)">
-      </v-list-item>
-    </v-list>
-  
-    <v-dialog v-model="modalOpen" max-width="500px">
-      <template v-slot:activator="{ on }"></template>
-      <v-card>
-        <v-card-title>{{ modalTitle }}</v-card-title>
-        <v-card-text>
-          <template v-if="modalImage">
-            <img :src="modalImage" alt="Modal Image" style="max-width: 100%;" />
-          </template>
-          <template v-else>
-            <p>{{ modalText }}</p>
-          </template>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" text @click="modalOpen = false">Cerrar</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </template>
+  <v-list :class="listDensity" :items="items">
+    <v-list-item @click="handleItemClick(item)">
+    </v-list-item>
+  </v-list>
 
+  <v-dialog v-model="modalOpen" max-width="500px">
+    <template v-slot:activator="{ on }"></template>
+    <v-card>
+      <v-card-title>{{ modalTitle }}</v-card-title>
+      <v-card-text>
+        <template v-if="modalImage">
+          <img :src="modalImage" alt="Modal Image" style="max-width: 100%;" />
+        </template>
+        <template v-else>
+          <p>{{ modalText }}</p>
+        </template>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" text @click="modalOpen = false">Cerrar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
 
 <script>
 import { ref } from 'vue';
-import { VDialog } from 'vuetify/lib/components';
+
+export const openModal = (type, content) => {
+  if (type === 'image') {
+    modalImage.value = content;
+    modalTitle.value = '';
+    modalText.value = '';
+  } else if (type === 'text') {
+    modalImage.value = '';
+    modalTitle.value = content.title;
+    modalText.value = content.text;
+  }
+  modalOpen.value = true;
+};
 
 export default {
-  components: {
-    VDialog
-  },
   setup() {
     const modalOpen = ref(false);
     const modalImage = ref('');
     const modalTitle = ref('');
     const modalText = ref('');
 
-    const openModal = (type, content) => {
-      if (type === 'image') {
-        modalImage.value = content;
-        modalTitle.value = '';
-        modalText.value = '';
-      } else if (type === 'text') {
-        modalImage.value = '';
-        modalTitle.value = content.title;
-        modalText.value = content.text;
-      }
-      modalOpen.value = true;
-    };
-
     return {
       modalOpen,
       modalImage,
       modalTitle,
-      modalText,
-      openModal
+      modalText
     };
   },
 
   methods: {
     handleItemClick(item) {
       if (item.modalType === 'image') {
-        this.openModal('image', item.modalContent.image);
+        openModal('image', item.modalContent.image);
       } else if (item.modalType === 'text') {
-        this.openModal('text', item.modalContent);
+        openModal('text', item.modalContent);
       }
     }
   }
